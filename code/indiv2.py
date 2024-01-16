e4c8be0eafca9537fc7229dc2593f6bc88cf68af
cnrde <conrade.tube@gmail.com>
2024-01-16 20:17:15 +0300

    commit

---
+++ code/indiv2.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    line = input("введите предложение: ")
+
+    for ind in range(len(line)-1):
+        if line[ind] == line[ind+1]:
+            print(ind+1, ind+2)
+            break
+    else:
+        print('Таких символов нет')
