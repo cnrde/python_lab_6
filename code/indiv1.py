bb46afa9ac9901d379018c517db018db990e194a
cnrde <conrade.tube@gmail.com>
2024-01-16 20:15:51 +0300

    commit

---
+++ code/indiv1.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    s = list(input("Напечатайте предложение: ").split())
+    c = input("Какой символ проверить на вхождение: ")
+    for i in range(len(s)):
+        if c in s[i]:
+            print(s[i])
